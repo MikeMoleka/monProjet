3b8f2c59c0d7a83ff05349993173eb093b06bb3a
MikeMoleka <mike.moleka@gmail.com>
2018-01-22 16:36:24 +0100

    creation formulaire

--- index.php
+++ index.php
@@ -1,6 +1,45 @@
-<?php  
-
-echo "Coucou";
-
-?>
-
+<html>
+</<!DOCTYPE html>
+<html>
+<head>
+	<title>formulaire</title>
+</head>
+<body>
+	<form method="post" action="insert.php">
+		<label>Entrez votre : </label>
+		<div>
+			<label>mail : </label>
+			<input type="text" name="mail">
+		</div>
+		
+		<div>
+			<label>Nom : </label>
+			<input type="text" name="nom">
+		</div>
+		<div>
+			<label>Prénom : 
+			<input type="text" name="prenom">
+			</label>
+		</div>
+		
+		<div>
+			<label>Adresse : 
+			<input type="text" name="adresse">
+		</label>
+		</div>
+		
+		<div>
+			<label>Age : 
+			<input type="text" name="age">
+		</label>
+		</div>
+		
+		<div>
+			<label>Téléphone : 
+			<input type="text" name="telephone">
+			</label>
+		</div>
+		<input type="submit">
+	</form>
+</body>
+</html>
