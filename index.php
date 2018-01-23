e1b9f21c3951151049969241e7cadd73d5fd6f86
MikeMoleka <mike.moleka@gmail.com>
2018-01-23 12:27:44 +0100

    redirection formulaire

--- index.php
+++ index.php
@@ -4,42 +4,38 @@
 <head>
 	<title>formulaire</title>
 </head>
-<body>
-	<form method="post" action="insert.php">
-		<label>Entrez votre : </label>
-		<div>
-			<label>mail : </label>
-			<input type="text" name="mail">
-		</div>
-		
-		<div>
-			<label>Nom : </label>
-			<input type="text" name="nom">
-		</div>
-		<div>
-			<label>Prénom : 
-			<input type="text" name="prenom">
-			</label>
-		</div>
-		
-		<div>
-			<label>Adresse : 
-			<input type="text" name="adresse">
-		</label>
-		</div>
-		
-		<div>
-			<label>Age : 
-			<input type="text" name="age">
-		</label>
-		</div>
-		
-		<div>
-			<label>Téléphone : 
-			<input type="text" name="telephone">
-			</label>
-		</div>
-		<input type="submit">
-	</form>
-</body>
+	<body>
+		<form method="post" action="insert.php">
+			<label>Entrez votre : </label>
+			<div>
+				<label>mail : </label>
+				<input type="text" name="mail" value="<?php print $_GET['mail']; ?>" >
+			</div>
+			
+			<div>
+				<label>Nom : </label>
+				<input type="text" name="nom" value="<?php print $_GET['nom']; ?>">
+			</div>
+			<div>
+				<label>Prénom : </label>
+				<input type="text" name="prenom" value="<?php print $_GET['prenom']; ?>">
+			</div>
+			
+			<div>
+				<label>Adresse : </label>
+				<input type="text" name="adresse" value="<?php print $_GET['adresse']; ?>">
+			</div>
+			
+			<div>
+				<label>Age : </label>
+				<input type="text" name="age" value="<?php print $_GET['age']; ?>">
+			</div>
+			
+			<div>
+				<label>Téléphone : </label>
+				<input type="text" name="telephone" value="<?php print $_GET['telephone']; ?>">
+			</div>
+			<input type="submit">
+		</form>
+	</body>
 </html>
--- insert.php
+++ insert.php
@@ -3,18 +3,34 @@
 	$array = array("mail" => $_POST['mail'], "nom" => $_POST['nom'], "prenom" => $_POST['prenom'], "adresse" => $_POST['adresse'], "age" => $_POST['age'], "telephone" => $_POST['telephone']);
 	$trouve = false;
 	$cpt = 0;
+	$error = 0;
+
+
+	$mail = $_POST['mail']; 
+	$nom = $_POST['nom'];
+	$prenom = $_POST['prenom'];
+	$age = $_POST['age'];
+	$telephone = $_POST['telephone'];
+	$adresse = $_POST['adresse'];
 
 	foreach ($array as $key => $value) {
 		$cpt++;
-		if( isset($value) && empty($value)) {
-			echo "le champs ";
-			echo $key;
-			echo " est vide";
-			echo"<br>";
-			$trouve = true;
-		}
-		else if ($cpt >= sizeof($array) && !$trouve){
-			echo "Formulaire envoyé";
+		if($value == NULL){
+			$error++;
 		}
 	}
+	if ($cpt == sizeof($array) && $error > 0) {
+		echo	"<script type=\"text/javascript\">
+					window.alert('$error champs vide veuillez remplir toutes les champs');
+					window.location.href = 'index.php?var=$key&mail=$mail&nom=$nom&prenom=$prenom&age=$age&telephone=$telephone&adresse=$adresse';
+					</script>";
+			exit;
+	}else if ($cpt == sizeof($array) && $error == 0){
+		echo	"<script type=\"text/javascript\">
+					window.alert('formulaire envoyé');
+					window.location.href = 'index.php?var=$key&mail=$mail&nom=$nom&prenom=$prenom&age=$age&telephone=$telephone&adresse=$adresse';
+					</script>";
+			exit;
+	}
+	
 ?>
